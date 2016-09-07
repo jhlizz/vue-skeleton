db132a8e5922c355b6699312b1d84c8dcfab8d26
zentoLu <491097990@qq.com>
2016-09-07 23:17:57 +0800

    add mockjs

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App';
 import VueRouter from 'vue-router';
 import configRouter from './router';
 
+// import MockRenderer from './mock';
+// MockRenderer();
+
 Vue.use(VueRouter);
 
 const router = new VueRouter();
---
+++ src/mock.js
@@ -0,0 +1,23 @@
+// import Mock from 'mockjs';
+
+
+
+
+// export default () => {
+
+//     const Render = (options) => {
+//         Mock.mock(options.url, options.type, options.tpl);
+//     };
+
+//     // 测试
+//     Render({
+//         url: '/featured/userinfo',
+//         type: 'get',
+//         tpl: {
+//             name: 'hahaha',
+//             age: 28
+//         }
+//     });
+
+
+// }
