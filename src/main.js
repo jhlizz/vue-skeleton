7e89131de7c8c5b5e5c03368244fa6cba089e12a
zentoLu <491097990@qq.com>
2016-09-08 18:11:07 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -2,16 +2,20 @@ import Vue from 'vue';
 import App from './App';
 import VueRouter from 'vue-router';
 import configRouter from './router';
-
+import MockInit from 'Vuex/mock';
 import VueResource from 'vue-resource';
 
-import MockRenderer from 'Vuex/mock';
-MockRenderer();
+if ('production' !== process.env.NODE_ENV) {
+    MockInit();
+}
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
 
-const router = new VueRouter();
+const router = new VueRouter({
+    hashbang: false,
+    history: true
+});
 
 configRouter(router);
 
